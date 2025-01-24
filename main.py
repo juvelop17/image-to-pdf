5426b95dcbba43fa4409b1b14ff75d80c76331ad
Junho Kim <juvelop17@gmail.com>
2025-01-24 23:43:58 +0900

    update program

--- main.py
+++ main.py
@@ -13,43 +13,6 @@ class WebpConverter:
         self.quality = quality
         self.method = 6  # Quality/speed trade-off (0=fast, 6=slower-better). Defaults to 4.
 
-    # def __convert_image_file(self, file_name, input_file, output_file):
-    #     try:
-    #         with Image.open(input_file) as img:
-    #             img.save(output_file, format='webp', quality=self.quality, method=self.method)
-    #             print(f"Converted {file_name} to {output_file}")
-    #     except Exception as e:
-    #         print(f"Failed to convert {file_name}: {e}")
-    #
-    # def convert_to_webp(self):
-    #     if not os.path.exists(self.output_path):
-    #         os.makedirs(self.output_path)
-    #
-    #     for file_name in sorted(os.listdir(self.input_path)):
-    #         if file_name.lower().endswith(self.supported_formats):
-    #             input_file = os.path.join(self.input_path, file_name)
-    #             output_file = os.path.join(self.output_path, f'{os.path.splitext(file_name)[0]}.webp')
-    #             self.__convert_image_file(file_name, input_file, output_file)
-    #
-    # def convert_to_pdf(self):
-    #     if not os.path.exists(self.output_path):
-    #         os.makedirs(self.output_path)
-    #
-    #     webp_files = [file_name for file_name in os.listdir(self.output_path) if file_name.endswith('.webp')]
-    #     webp_files.sort()
-    #
-    #     webp_images = []
-    #     for file_name in webp_files:
-    #         file_path = os.path.join(self.output_path, file_name)
-    #         with Image.open(file_path) as img:
-    #             if img.mode != 'RGBA':
-    #                 img = img.convert('RGBA')
-    #             webp_images.append(img)
-    #
-    #     if webp_images:
-    #         output_pdf = os.path.join(self.output_path, 'out.pdf')
-    #         webp_images[0].save(output_pdf, save_all=True, append_images=webp_images[1:], quality=self.quality)
-
     def __convert_image(self, file_name, input_file):
         buffer = BytesIO()
         try:
@@ -75,27 +38,17 @@ class WebpConverter:
         return converted_images
 
     def convert_to_pdf(self, buffers):
+        output_pdf = os.path.join(self.output_path, 'output.pdf')
         images = []
         for buffer in buffers:
             img = Image.open(buffer)
             images.append(img)
-
-            # with Image.open(buffer) as img:
-            # if img.mode != 'RGBA':
-            #     img = img.convert('RGBA')
-            # images.append(img)
-            # output_pdf = os.path.join(self.output_path, 'output.pdf')
-            # img.save(output_pdf, save_all=True, append_images=images[1:])
-            # print(f"PDF created successfully with {len(images)} pages: {output_pdf}")
-
-        output_pdf = os.path.join(self.output_path, 'output.pdf')
         try:
             images[0].save(output_pdf, save_all=True, append_images=images[1:])
             print(f"PDF created successfully with {len(images)} pages: {output_pdf}")
         except Exception as e:
             print(f"Failed to convert {output_pdf}: {e}")
             print(e.__traceback__)
-
         for img in images:
             img.close()
 

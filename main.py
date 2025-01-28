971457738b58e4fc69c899e1289953eb8fa9f4be
Junho Kim <juvelop17@gmail.com>
2025-01-29 00:13:47 +0900

    Add Progress Tracking Feature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from io import BytesIO
 from multiprocessing import Pool, cpu_count
 
+import tqdm
 from PIL import Image, ImageEnhance, ImageFilter
 
 
@@ -20,8 +21,8 @@ class PDFConverter:
         self.max_width = 0
         self.max_height = 0
 
-    def check_image_size(self, input_file):
-        with Image.open(input_file) as image:
+    def check_image_size(self, file_path):
+        with Image.open(file_path) as image:
             image = self.__crop_image(image)
             width, height = image.size
             return width, height
@@ -43,7 +44,6 @@ class PDFConverter:
         width, height = image.size
         if (self.max_width / width > 1.1) or (self.max_height / height > 1.1):
             self.anomaly_detected_size = True
-        # print(f'current image size ratio: {round(self.max_width / width, 3)}, {round(self.max_height / height, 3)}')
         left = (self.max_width - width) // 2
         top = (self.max_height - height) // 2
         new_image = Image.new('RGB', (self.max_width, self.max_height), (255, 255, 255))
@@ -65,7 +65,6 @@ class PDFConverter:
                    progressive=True)
 
     def convert_image(self, file_path):
-        print(f"Processing {file_path}")
         temp_image = BytesIO()
         try:
             with Image.open(file_path) as image:
@@ -81,26 +80,37 @@ class PDFConverter:
             print(f"Failed to convert {file_path}: {e}")
         return temp_image
 
-    def __convert_to_jpg(self):
+    def __check_images(self, file_list):
+        print(f"Checking {len(file_list)} images...")
+        widths = []
+        heights = []
+        pool_size = cpu_count()
+        file_paths = list(map(lambda file_name: os.path.join(self.input_path, file_name), file_list))
+        with Pool(pool_size) as pool:
+            results = pool.imap(self.check_image_size, file_paths)
+            for result in tqdm.tqdm(results, total=len(file_paths)):
+                widths.append(result[0]), heights.append(result[1])
+        self.max_width = max(widths)
+        self.max_height = max(heights)
+        return file_paths, pool_size
+
+    def __convert_images(self, file_list):
+        print(f"Processing {len(file_list)} images...")
         images = []
+        pool_size = cpu_count()
+        file_paths = list(map(lambda file_name: os.path.join(self.input_path, file_name), file_list))
+        with Pool(pool_size) as pool:
+            results = pool.imap(self.convert_image, file_paths)
+            for result in tqdm.tqdm(results, total=len(file_paths)):
+                images.append(result)
+        return images
+
+    def __convert_to_jpg(self):
         condition = lambda file_name: file_name.lower().endswith(self.supported_formats)
         file_list = list(filter(condition, os.listdir(self.input_path)))
         file_list.sort()
-
-        pool_size = cpu_count()
-        with Pool(pool_size) as pool:
-            file_paths = map(lambda _file_name: os.path.join(self.input_path, _file_name), file_list)
-            async_result = pool.map_async(self.check_image_size, file_paths)
-            results = async_result.get()
-            widths, heights = zip(*results)
-            self.max_width = max(widths)
-            self.max_height = max(heights)
-        with Pool(pool_size) as pool:
-            file_paths = map(lambda _file_name: os.path.join(self.input_path, _file_name), file_list)
-            async_result = pool.map_async(self.convert_image, file_paths)
-            results = async_result.get()
-            images.extend(results)
-        return images
+        self.__check_images(file_list)
+        return self.__convert_images(file_list)
 
     def __convert_to_pdf(self, temp_images):
         output_pdf = os.path.join(self.output_path, 'output.pdf')
@@ -110,7 +120,7 @@ class PDFConverter:
             images.append(img)
         if images:
             try:
-                print(f"creating PDF file... {len(images)} pages: {output_pdf}")
+                print(f"Creating PDF file... {len(images)} pages: {output_pdf}")
                 images[0].save(output_pdf, save_all=True, append_images=images[1:])
                 print(f"PDF created successfully with {len(images)} pages: {output_pdf}")
             except Exception as e:
